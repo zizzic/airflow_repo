7771161719f94b95c57e895984d1731fd0c1a79b
poriz <areacmzl@gmail.com>
2024-02-21 10:23:52 +0900

    [feat] add glue code

--- dataCollector/glue&athena/glue_airflow.py
+++ dataCollector/glue&athena/glue_airflow.py
@@ -1,50 +1,70 @@
-from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.providers.amazon.aws.operators.glue import AwsGlueJobOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
+from airflow.providers.amazon.aws.hooks.s3 import S3Hook
+from datetime import datetime, timedelta
+from jinja2 import Template
 
 
-# 전날 날짜를 계산하는 함수
-def set_glue_crawler_params():
-    yesterday = datetime.now() - timedelta(1)
-    date_str = yesterday.strftime("%Y-%m-%d")
-    # 여기서 필요한 파라미터를 설정할 수 있습니다.
-    # 예: S3 경로 내 날짜 기반 폴더
-    return {"date": date_str}
+def upload_script_to_s3(bucket_name, s3_key, template_path, aws_conn_id, **kwargs):
+    # S3Hook 인스턴스 생성
+    s3_hook = S3Hook(aws_conn_id=aws_conn_id)
 
+    # Jinja 템플릿 파일 읽기
+    with open(template_path, 'r') as file:
+        template = Template(file.read())
 
-default_args = {
-    "owner": "airflow",
-    "depends_on_past": False,
-    "email_on_failure": False,
-    "email_on_retry": False,
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
-}
+    # 템플릿 렌더링
+    rendered_script = template.render(**kwargs)
+
+    # 렌더링된 스크립트를 S3에 업로드
+    s3_hook.load_string(
+        string_data=rendered_script,
+        bucket_name=bucket_name,
+        key=s3_key,
+        replace=True
+    )
 
 with DAG(
-    "glue_crawler_dag",
-    default_args=default_args,
-    description="DAG for running AWS Glue Crawler",
-    schedule_interval=timedelta(days=1),  # 매일 실행
-    start_date=datetime(2022, 1, 1),
+    "get_raw_data",
+    default_args={
+        "owner": "airflow",
+        "depends_on_past": False,
+        "start_date": datetime(2024, 1, 17),
+        "retries": 0,
+        "retry_delay": timedelta(minutes=5),
+    },
+    schedule_interval="0 * * * *",
     catchup=False,
 ) as dag:
 
-    # 파라미터 설정 Task
-    set_params = PythonOperator(
-        task_id="set_glue_crawler_params",
-        python_callable=set_glue_crawler_params,
+    bucket_name = "de-2-1-bucket"
+    local_path = f'./glue_script.py'
+    year = "{{ data_interval_end.year }}"
+    month = "{{ data_interval_end.month }}"
+    day = "{{ data_interval_end.day }}"
+
+    upload_script = PythonOperator(
+            task_id='upload_script_to_s3',
+            python_callable=upload_script_to_s3,
+            op_kwargs={
+                "bucket_name": bucket_name,
+                "aws_conn_id": "aws_conn_id",
+                "s3_key": f"source/script/glue_script.py",
+                'template_path': './glue_template.py',
+                # into template
+                'input_path': f's3://de-2-1-bucket/source/json/table_name=raw_live_viewer/year={year}/month={month}/day={day}/',
+                'output_path': 's3://de-2-1-bucket/source/parquet/', # do change_path
+            }
+        )
+
+    run_glue_job = GlueJobOperator(
+        task_id='run_glue_job',
+        job_name='DE-2-1-testjob',
+        script_location='s3://your_bucket_name/path/to/your_script.py',
+        aws_conn_id='aws_conn_id',
+        region_name='ap-northeast-2',
+        dag=dag,
     )
 
-    # Glue Crawler 실행 Task
-    run_glue_crawler = AwsGlueJobOperator(
-        task_id="run_glue_crawler",
-        job_name="your_glue_crawler_name",  # AWS Glue에서 설정한 Crawler 이름
-        script_location="s3://your-bucket/path/to/glue/script",  # Glue 스크립트 위치
-        region_name="your-aws-region",
-        # 필요한 경우 아래에 추가 파라미터를 전달할 수 있습니다.
-        # 예: '--additional-python-modules', 'pyarrow==2, pandas'
-    )
-
-    set_params >> run_glue_crawler
+upload_script >> run_glue_job
---
+++ dataCollector/glue&athena/glue_template.py
@@ -0,0 +1,38 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+
+# SparkContext와 GlueContext 초기화
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+# Job 초기화 (Job Bookmark 활성화 포함)
+job = Job(glueContext)
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+job.init(args['JOB_NAME'], args)
+
+# S3에서 데이터를 읽어오는 부분
+datasource = glueContext.create_dynamic_frame.from_options(
+    's3',
+    {'paths': ['{{ input_path }}'], 'recurse':True},
+    format='json',
+    transformation_ctx='datasource',
+    additional_options={"bookmarkEnabled":"true"}
+)
+
+# Parquet으로 변환하여 S3에 저장
+glueContext.write_dynamic_frame.from_options(
+    frame=datasource,
+    connection_type='s3',
+    connection_options={'path': '{{ output_path }}'},
+    format='parquet',
+    transformation_ctx='datasource'
+)
+
+
+# Job Bookmark의 상태를 최종적으로 커밋
+job.commit()
