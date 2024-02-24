d9f4a5df6f04859c9efd9c2ea833d34e6069c39f
lyh <mediwind@gmail.com>
2024-02-24 19:53:50 +0900

    [feat] make Glue DAG for game_rating

--- dataCollector/glue&athena/glue_game_rating_template.py
+++ dataCollector/glue&athena/glue_game_rating_template.py
@@ -27,17 +27,20 @@ datasource = glueContext.create_dynamic_frame.from_options(
 )
 
 # 전처리를 위해 DF로 변환하기
-game_ccu_datasource = datasource.toDF()
+game_rating_datasource = datasource.toDF()
 
 # 최상위 레벨의 key를 중심으로 explode하기
-df = game_ccu_datasource.select(
-    explode(game_ccu_datasource.raw_game_ccu).alias("raw_game_ccu")
+df = game_rating_datasource.select(
+    explode(game_rating_datasource.raw_game_rating).alias("raw_game_rating")
 )
 
 df = df.select(
-    col("raw_game_ccu.game_id").alias("GAME_ID"),
-    lit("{{ collect_time }}").alias("COLLECT_TIME"),
-    col("raw_game_ccu.player_count").alias("GAME_CCU"),
+    col("raw_game_rating.game_id").alias("GAME_ID"),
+    lit("{{ collect_time }}").alias("COLLECT_DATE"),
+    col("raw_game_rating.recent_positive_num").alias("RECENT_POSITIVE_NUM"),
+    col("raw_game_rating.recent_positive_percent").alias("RECENT_POSITIVE_PERCENT"),
+    col("raw_game_rating.all_positive_num").alias("ALL_POSITIVE_NUM"),
+    col("raw_game_rating.all_positive_percent").alias("ALL_POSITIVE_PERCENT"),
 )
 
 dynamic_frame = DynamicFrame.fromDF(df, glueContext, "dynamic_frame")
