72ba984d60c85ad979b8e8680d217664d873d37f
poriz <areacmzl@gmail.com>
2024-02-24 12:22:10 +0900

    [feat] live_viewer_template.py change code, add platform

--- dataCollector/glue&athena/live_viewer_template.py
+++ dataCollector/glue&athena/live_viewer_template.py
@@ -35,25 +35,43 @@ afreeca_source = datasource_df.select("stream_data.afreeca").select(explode("afr
 # chzzk_source.printSchema()
 chzzk_df = chzzk_source.select(
     col("col.streamer_id").alias("STREAMER_ID"),
-    col("col.chzzk_s_id").alias("CHZ_STEAMER_ID"),
-    col("col.content.liveTitle").alias("CHZ_BROADCAST_TITLE"),
-    col("col.content.liveCategoryValue").alias("CHZ_GAME_CODE"),
-    col("col.content.concurrentUserCount").alias("CHZ_VIEWER_NUM"),
+    col("col.content.liveID").alias("BROADCAST_ID"),
+    col("col.content.liveTitle").alias("BROADCAST_TITLE"),
+    col("col.content.liveCategoryValue").alias("GAME_CODE"),
+    col("col.content.concurrentUserCount").alias("VIEWER_NUM"),
 )
+# add platform
+chzzk_df = chzzk_df.withColumn("PLATFORM", lit("chzzk"))
 # chzzk_df.show()
 
 # afreeca_source.printSchema()
 afreeca_df = afreeca_source.select(
     col("col.streamer_id").alias("STREAMER_ID"),
-    col("col.afc_s_id").alias("AFC_STEAMER_ID"),
     col("col.live_status.BNO").alias("BROADCAST_ID"),
-    col("col.live_status.TITLE").alias("AFC_BROADCAST_TITLE"),
-    col("col.live_status.CATE").alias("AFC_GAME_CODE"),
-    col("col.broad_info.broad.current_sum_viewer").alias("AFC_VIEWER_NUM"),
+    col("col.live_status.TITLE").alias("BROADCAST_TITLE"),
+    col("col.live_status.CATE").alias("GAME_CODE"),
+    col("col.broad_info.broad.current_sum_viewer").alias("VIEWER_NUM"),
 )
+afreeca_df = afreeca_df.withColumn("PLATFORM", lit("afreeca"))
 # afreeca_df.show(truncate=False)
 
-result_df = chzzk_df.join(afreeca_df, "STREAMER_ID", "outer")
+
+result_df = chzzk_df.join(
+    afreeca_df,
+    [
+        "STREAMER_ID",
+        "BROADCAST_ID",
+        "BROADCAST_TITLE",
+        "GAME_CODE",
+        "PLATFORM",
+        "VIEWER_NUM",
+    ],
+    "outer",
+)
+
+# 스키마 정보를 로깅
+print("Schema Information:")
+result_df.printSchema()
 
 dynamicframe = DynamicFrame.fromDF(result_df, glueContext, "dynamicframe")
 
@@ -66,7 +84,6 @@ glueContext.write_dynamic_frame.from_options(
     format="parquet",
     transformation_ctx="datasource",
 )
-
 
 # Job Bookmark의 상태를 최종적으로 커밋
 job.commit()
