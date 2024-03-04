7bb003802914b5f0c2431b7054deffdeb82bb3f8
poriz <areacmzl@gmail.com>
2024-03-05 00:10:41 +0900

    [feat] add anal_broadcast.py, fix-live_viewer_script.py template(x)

---
+++ dags/elt/anal_broadcast.py
@@ -0,0 +1,120 @@
+from airflow import DAG
+from airflow.decorators import task
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+from datetime import datetime, timedelta
+import slack
+
+
+def connect_to_redshift():
+    db_hook = PostgresHook(postgres_conn_id="aws_redshift_conn_id")
+    conn = db_hook.get_conn()
+    conn.autocommit = True
+
+    return conn.cursor()
+
+
+@task
+def elt():
+    # 1. reshift external_raw_data 스키마
+    cur = connect_to_redshift()
+    print("Successfully connected to Redshift")
+    conn = cur.connection
+
+    try:
+        # Start a new transaction
+        conn.autocommit = False
+
+        # analytics.ANAL_YSD_GAME_CCU 테이블의 모든 데이터 삭제
+        sql = """
+                DELETE FROM analytics.ANAL_BROADCAST;
+                """
+        cur.execute(sql)
+        print("Successfully deleted all data from analytics.ANAL_BROADCAST")
+
+        # SELECT 쿼리의 결과를 analytics.ANAL_YSD_GAME_CCU 테이블에 삽입
+        sql = """
+            INSERT INTO analytics.ANAL_BROADCAST(STREAMER_NM, BROADCAST_ID, GAME_NM, PLATFORM, AVG_VIEWER_NUM, BROADCAST_START_TIME, BROADCAST_END_TIME, GAME_DURATION, CREATED_DATE)
+            WITH ParsedData AS (
+                SELECT *, live_collect_time::TIMESTAMPTZ AS parsed_time
+                FROM external_raw_data.table_name_raw_live_viewer
+            ),
+            RankedData AS(
+                SELECT 
+                    *,
+                    LAG(parsed_time, 1) OVER (
+                        PARTITION BY streamer_id, broadcast_id, game_code
+                        ORDER BY parsed_time
+                    ) AS prev_timestamp
+                FROM ParsedData
+            ),
+            GroupedData AS(
+                SELECT *,
+                    CASE
+                        WHEN parsed_time - INTERVAL '5' MINUTE > prev_timestamp THEN 1
+                        ELSE 0
+                    END AS is_new_group,
+                    COALESCE(NULLIF(game_code, ''), 'talk') AS normalized_game_code
+                FROM RankedData
+            ),
+            GroupIDs AS (
+                SELECT *,
+                    SUM(is_new_group) OVER (
+                        PARTITION BY streamer_id, broadcast_id, normalized_game_code
+                        ORDER BY parsed_time
+                        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+                    ) AS group_id,
+                    normalized_game_code AS n_game_code
+                FROM GroupedData
+            )
+            SELECT
+                s_info.streamer_nm AS STREAMER_NM,
+                g_ids.broadcast_id AS BROADCAST_ID,
+                COALESCE(g_info.game_nm,g_ids.n_game_code) AS GAME_NM,
+                g_ids.platform AS PLATFORM,
+                AVG(g_ids.viewer_num)::integer AS AVG_VIEWER_NUM,
+                MIN(g_ids.parsed_time) AS start_time,
+                MAX(g_ids.parsed_time) AS end_time,
+                (EXTRACT(EPOCH FROM MAX(g_ids.parsed_time)) - EXTRACT(EPOCH FROM MIN(g_ids.parsed_time))) / 60 AS GAME_DURATION,
+                CURRENT_DATE AS created_date
+            FROM GroupIDs AS g_ids
+            LEFT JOIN external_raw_data.streamer_info AS s_info ON s_info.streamer_id = g_ids.streamer_id
+            LEFT JOIN external_raw_data.game_info AS g_info
+            ON g_ids.n_game_code = g_info.chz_game_code OR LTRIM(g_ids.n_game_code, '0') = g_info.afc_game_code
+            GROUP BY STREAMER_NM, BROADCAST_ID, GAME_NM, PLATFORM, g_ids.group_id, g_ids.n_game_code
+            ORDER BY STREAMER_NM, BROADCAST_ID, start_time; 
+            """
+        cur.execute(sql)
+        print("Successfully inserted data into analytics.ANAL_BROADCAST")
+
+        # 트랜잭션 commit
+        conn.commit()
+        print("Successfully committed the transaction")
+
+    except Exception as e:
+        # Rollback
+        print("Error occurred. Start to rollback", e)
+        conn.rollback()
+        raise
+
+    finally:
+        # Close the cursor and connection
+        cur.close()
+        conn.close()
+        print("Connection to Redshift is closed")
+
+
+with DAG(
+    dag_id="ELT_ANAL_BROADCAST",
+    start_date=datetime(2024, 1, 1),
+    catchup=False,
+    tags=["ELT", "analytics", "game_ccu"],
+    schedule_interval="0 2 * * *",  # 매일 11시
+    default_args={
+        "retries": 3,
+        "retry_delay": timedelta(minutes=5),
+        "on_failure_callback": slack.on_failure_callback,
+    },
+) as dag:
+
+    elt()
---
+++ dataCollector/glue&athena/live_viewer_script.py
@@ -0,0 +1,85 @@
+## live_viewer glue script template
+
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from pyspark.sql.functions import *
+from awsglue.dynamicframe import DynamicFrame
+
+# SparkContext와 GlueContext 초기화
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+
+# Job 초기화 (Job Bookmark 활성화 포함)
+job = Job(glueContext)
+args = getResolvedOptions(sys.argv, ['JOB_NAME', 'input_path', 'output_path'])
+job.init(args["JOB_NAME"], args)
+
+input_path = args['input_path']
+output_path = args['output_path']
+
+# S3에서 데이터를 읽어오는 부분
+datasource = glueContext.create_dynamic_frame.from_options(
+    "s3",
+    {"paths": [input_path], "recurse": True},
+    format="json",
+    transformation_ctx="datasource",
+)
+# 데이터 가공
+datasource_df = datasource.toDF()
+
+chzzk_source = datasource_df.select("stream_data.chzzk").select(explode("chzzk"))
+afreeca_source = datasource_df.select("stream_data.afreeca").select(explode("afreeca"))
+
+# chzzk_source.printSchema()
+chzzk_df = chzzk_source.select(
+    col("col.streamer_id").alias("STREAMER_ID"),
+    col("col.content.liveID").alias("BROADCAST_ID"),
+    col("col.current_time").alias("LIVE_COLLECT_TIME"),
+    col("col.content.liveTitle").alias("BROADCAST_TITLE"),
+    col("col.content.liveCategoryValue").alias("GAME_CODE"),
+    col("col.content.concurrentUserCount").alias("VIEWER_NUM"),
+)
+# add platform
+chzzk_df = chzzk_df.withColumn("PLATFORM", lit("chzzk"))
+# chzzk_df.show()
+
+# afreeca_source.printSchema()
+afreeca_df = afreeca_source.select(
+    col("col.streamer_id").alias("STREAMER_ID"),
+    col("col.live_status.BNO").alias("BROADCAST_ID"),
+    col("col.current_time").alias("LIVE_COLLECT_TIME"),
+    col("col.live_status.TITLE").alias("BROADCAST_TITLE"),
+    col("col.live_status.CATE").alias("GAME_CODE"),
+    col("col.broad_info.broad.current_sum_viewer").alias("VIEWER_NUM"),
+)
+afreeca_df = afreeca_df.withColumn("PLATFORM", lit("afreeca"))
+# afreeca_df.show(truncate=False)
+
+result_df = chzzk_df.union(afreeca_df)
+
+result_df = result_df.withColumn("GAME_CODE", when(col("GAME_CODE").isNull(), lit("talk")).otherwise(col("GAME_CODE")))
+
+# 스키마 정보를 로깅
+print("Schema Information:")
+result_df.printSchema()
+
+# "PLATFORM" 컬럼을 기준으로 파티션을 구성
+partitioned_df = result_df.repartition("PLATFORM")
+
+# 파티션된 Spark DataFrame을 DynamicFrame으로 변환
+partitioned_dynamic_frame = DynamicFrame.fromDF(partitioned_df, glueContext, "partitioned_dynamic_frame")
+
+
+# Parquet으로 변환하여 S3에 저장
+glueContext.write_dynamic_frame.from_options(
+    frame=partitioned_dynamic_frame,
+    connection_type="s3",
+    connection_options={"path": output_path},
+    format="parquet",
+    transformation_ctx="datasource",
+)
